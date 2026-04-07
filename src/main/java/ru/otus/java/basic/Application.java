2fb72511dc71515e814878c393684fd7c6a6bfc3
ciscomsk <kuznetzoff@gmail.com>
2026-04-07 14:23:34 +0300

    Добавлен метод demo

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -1,7 +1,20 @@
 package ru.otus.java.basic;
 
+import java.util.stream.IntStream;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        demo();
+    }
+
+    public static void demo() {
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+        // =
+        System.out.println("-----");
+        IntStream.rangeClosed(1, 3).forEach(System.out::println);
+        System.out.println("-----");
     }
 }
