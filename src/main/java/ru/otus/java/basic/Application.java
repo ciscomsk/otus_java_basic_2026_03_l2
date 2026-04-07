152240e6780bd8963b4469ce785804e7cea135e9
ciscomsk <kuznetzoff@gmail.com>
2026-04-07 17:02:17 +0300

    dev-1 начало работы

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -32,4 +32,9 @@ public class Application {
         IntStream.rangeClosed(1, 3).forEach(System.out::println);
         System.out.println("000-");
     }
+
+    public static void featureDevOne() {
+        System.out.println("feature dev one start");
+        System.out.println("feature dev one end");
+    }
 }
