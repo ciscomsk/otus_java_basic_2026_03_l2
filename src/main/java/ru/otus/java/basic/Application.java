32736864d31e6f11dc125a0a50064119280e094a
ciscomsk <kuznetzoff@gmail.com>
2026-04-07 14:27:38 +0300

    Восстановили метод demo

--- src/main/java/ru/otus/java/basic/Application.java
+++ src/main/java/ru/otus/java/basic/Application.java
@@ -5,7 +5,18 @@ import java.util.stream.IntStream;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        demo();
         demo2();
+    }
+
+    public static void demo() {
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+        // =
+        System.out.println("-----");
+        IntStream.rangeClosed(1, 3).forEach(System.out::println);
+        System.out.println("-----");
     }
 
     public static void demo2() {
